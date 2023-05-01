e7ea95411cc53bedb3d793517715e80a3abc65c0
michaelorina <orinamichael4@gmail.com>
2023-05-01 12:05:57 +0300

    create interface SidebarItemProps

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -1,4 +1,17 @@
-const SidebarItem = () => {
+import {IconType} from 'react-icons';
+
+interface SidebarItemProps {
+    label: string;
+    href?: string;
+    icon: IconType
+    onClick?: () => void;
+}
+const SidebarItem: React.FC<SidebarItemProps>= ({
+    label,
+    href,
+    icon,
+    onClick
+    }) => {
     return (
         <div></div>
     );
