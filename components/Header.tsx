2f36c94b1a3dad1e9176d9a717b488f2799cc291
michaelorina <orinamichael4@gmail.com>
2023-05-01 13:32:02 +0300

    create handleBack function

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,12 @@
 import { useRouter } from "next/router";
+import { useCallback } from "react";
 
 const Header = () =>{
     const router = useRouter();
+
+    const handleBack = useCallback(() => {
+        router.back();
+    }, [router]);
     return(
         <div></div>
     );
