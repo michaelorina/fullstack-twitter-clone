86b87c3fe3c05d577a4bd552692f996d5ff0ca5e
michaelorina <orinamichael4@gmail.com>
2023-05-01 13:43:44 +0300

    create Header Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps{
     label: string;
@@ -14,7 +15,25 @@ const Header: React.FC<HeaderProps> = ({ label, showBackArrow}) =>{
         router.back();
     }, [router]);
     return(
-        <div></div>
+        <div className="border-p-[1-px] border-neutral-800 p-5">
+            <div className="flex flex-row items-center gap-2">
+                {
+                    showBackArrow && (
+                        <BiArrowBack
+                            onClick={handleBack}
+                            color="white"
+                            size={20}
+                            className="
+                                cursor-pointer
+                                hover-opacity-70
+                                transition
+                            "
+                        />
+                    )
+                }
+                <h1 className="text-white text-xl font-semibold">{label}</h1>
+            </div>
+        </div>
     );
 }
 export default Header;
