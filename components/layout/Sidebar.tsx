dc7967813ee026bd3322ab2ce6a6cbca265b6351
michaelorina <orinamichael4@gmail.com>
2023-05-01 11:25:55 +0300

    create objects

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,22 @@
+import {BsBellFill, BsHouseFill} from 'react-icons/bs';
+import {FaUser} from 'react-icons/fa';
 const Sidebar = () => {
 
     const items = [
         {
-
+            label: 'Home',
+            href: '/',
+            icon: BsHouseFill
+        },
+        {
+            label: 'Notifications',
+            href: '/notifications',
+            icon: BsBellFill
+        },
+        {
+            label: 'Profile',
+            href: '/users/123',
+            icon: FaUser
         }
     ];
     return(
