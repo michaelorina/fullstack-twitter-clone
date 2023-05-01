ad91e44ebc7a52048ce33ca8fe4c73eb4f9468a9
michaelorina <orinamichael4@gmail.com>
2023-05-01 11:41:48 +0300

    style SidebarLogo

--- components/layout/SidebarLogo.tsx
+++ components/layout/SidebarLogo.tsx
@@ -3,7 +3,19 @@ import { useRouter } from "next/router";
 const SidebarLogo = () => {
     const router = useRouter();
     return (
-        <div></div>
+        <div className="
+        rounded-full
+        h-14
+        w-14
+        p-4
+        flex
+        items-center
+        justify-center
+        hover:bg-blue-300
+        hover:bg-opacity-10
+        cursor-pointer
+        transition
+        "></div>
     );
 }
 export default SidebarLogo;
