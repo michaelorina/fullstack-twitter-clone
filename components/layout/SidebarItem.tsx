e45415482cf9537267534b79f78b3ae23a7cea36
michaelorina <orinamichael4@gmail.com>
2023-05-01 12:10:47 +0300

    create divs and style SidebarItem component

--- components/layout/SidebarItem.tsx
+++ components/layout/SidebarItem.tsx
@@ -13,7 +13,22 @@ const SidebarItem: React.FC<SidebarItemProps>= ({
     onClick
     }) => {
     return (
-        <div></div>
+        <div className="flex flex-row items-center">
+            <div className="
+            relative
+            rounded-full
+            h-14
+            w-14
+            flex
+            items-center
+            justify-center
+            p-4
+            hover:bg-slate-300
+            hover:bg-opacity-10
+            cursor-pointer
+            lg:hidden
+            "></div>
+        </div>
     );
 }
 export default SidebarItem;
