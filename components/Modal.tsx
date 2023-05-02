d74850c2df63f6b660003b27e729e3caa1fa6249
michaelorina <orinamichael4@gmail.com>
2023-05-02 12:14:09 +0300

    create interface, handleClose function

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,4 +1,33 @@
-const Modal = () => {
+import React, { useCallback } from "react";
+import { on } from "stream";
+
+interface ModalProps{
+    isOpen?: boolean;
+    onClose: () => void;
+    onSubmit: () => void;
+    title?: string;
+    body?: React.ReactElement;
+    footer?: React.ReactElement;
+    actionLabel: string;
+    disabled?: boolean;
+}
+const Modal: React.FC<ModalProps> = ({
+    isOpen,
+    onClose,
+    onSubmit,
+    title,
+    body,
+    footer,
+    actionLabel,
+    disabled
+}) => {
+    const handleClose = useCallback(()=> {
+        if (disabled){
+            return;
+        }
+
+        onClose();
+    },[disabled, onClose]);
     return(
         <div></div>
     );
